0c765d114888486901dde8e3cbb1516c24f7a70b
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2026-02-07 22:33:25 -0300

    Atualizar o DAOAbstrato.java

---
+++ Lista05-BD/JAVA/MODELO/DAO/DAOAbstrato.java
@@ -0,0 +1,32 @@
+package modelo.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DAOAbstrato implements AutoCloseable {
+
+    private final String url = "jdbc:mysql://10.225.0.4/20231164010027_CONSUTORIA";
+    private final String usuario = "20231164010027";
+    private final String senha = "20231164010027+lima";
+
+    protected Connection conexao;
+
+    public AbstratoDAO() {
+        try {
+            conexao = DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            conexao.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
